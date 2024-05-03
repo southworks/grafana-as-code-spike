57cc34fb8cb86bf92be47e94cd5f6790f9c5ef64
Elian Temes Sanchez <elian.temes@southworks.com>
2024-05-02 23:18:27 -0300

    trying to make terraform work with the grafana cloud instance

--- dashboard.tf
+++ dashboard.tf
@@ -1,8 +1,9 @@
 resource "grafana_dashboard" "test" {
-  provider = grafana
-  org_id = grafana_organization.swks.org_id
+  depends_on = [ grafana_cloud_stack_service_account_token.cloud_sa_token ]
+  provider = grafana.my_stack
   folder = grafana_folder.swks_test_folder.id
   overwrite = true
-  for_each    = fileset("${path.module}/dashboards", "*.json")
-  config_json = file("${path.module}/dashboards/${each.key}")
+  org_id = grafana_organization.swks.org_id
+  for_each    = fileset("${path.module}/templates/dashboards", "*.json")
+  config_json = file("${path.module}/templates/dashboards/${each.key}")
 }
